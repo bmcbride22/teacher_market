7592b7b76785ebe6bb6e5058aac51b5f6162718f
Bassel Diab <kit2011@msn.com>
2022-12-24 17:18:31 +0800

    create_model_Transactions_ressouce

---
+++ app/models/transactions_ressource.rb
@@ -0,0 +1,4 @@
+class TransactionsRessource < ApplicationRecord
+    belongs_to :resource
+            
+end
---
+++ db/migrate/20221224091548_create_transactions_ressources.rb
@@ -0,0 +1,9 @@
+class CreateTransactionsRessources < ActiveRecord::Migration[7.0]
+  def change
+    create_table :transactions_ressources do |t|
+      t.references :resource, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
