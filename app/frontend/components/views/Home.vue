b7292c6993500001dae015db7ec6b363dd352a2f
Brendan McBride <brendan.mcmath@gmail.com>
2022-12-24 11:35:36 +0800

    add navbar html partial and create a navbar vue component in case I can switch to it and use it later

--- app/frontend/components/views/Home.vue
+++ app/frontend/components/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h3>This is the homepage component</h3>
+
     <h6>
       You can find me in: <span class="px-2 py-1 bg-slate-100">{{ dir }}</span>
     </h6>
--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -2,7 +2,11 @@ import "./main.scss";
 
 // Setup vue as below to sprinkle Vue components on rails views
 import { createApp } from "vue/dist/vue.esm-bundler";
+// import { Menu, MenuButton, MenuItem, MenuItems, Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
+// import { MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
+// import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
 import Home from "../components/views/Home.vue";
+import Navbar from "../components/views/Navbar.vue";
 
 const app = createApp({
   data() {
@@ -14,4 +18,6 @@ const app = createApp({
 
 // import (and use) the components one by one
 app.component("Home", Home);
+app.component("Navbar", Navbar);
+app.component("Test", Test);
 app.mount("#vue");
