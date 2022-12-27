3acc92b149298db50d9c0ac83f63bf7e9c358881
Brendan McBride <brendan.mcmath@gmail.com>
2022-12-27 10:49:25 +0800

    renamed the users/show as profiles/show to avoid devise controller namespace conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  get 'profiles/show/:id', to: 'profiles#show', as: :profiles_show
   resources :resources
-  get '/users/:id' => 'users#show', as: :show_user
-  devise_for :users
 
+  devise_for :users
   root to: 'pages#home'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
